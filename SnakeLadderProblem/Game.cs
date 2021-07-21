008d128fc77ab67101208cd86134f8d7896af784
omkargb <omkargb107@gmail.com>
2021-07-21 21:59:51 +0530

    [omkargb] Refractor : Rolling a die implemented

--- SnakeLadderProblem/Game.cs
+++ SnakeLadderProblem/Game.cs
@@ -10,6 +10,10 @@ namespace SnakeLadderProblem
         {
             int playerPosition = 0;
             Console.WriteLine(" Player Position : "+playerPosition);
+            
+            Random random = new Random();
+            int diceValue = random.Next(1,7);     // generates between 1 to 6
+            Console.WriteLine(" Rolling a die. Value is : "+diceValue);
         }
     }
 }
