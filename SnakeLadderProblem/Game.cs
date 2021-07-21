b692f38f1a24e20c4158209faecf0eb816a457ed
omkargb <omkargb107@gmail.com>
2021-07-21 20:36:00 +0530

    [omkargb] Add : Initiated player position with 0

---
+++ SnakeLadderProblem/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderProblem
+{
+    class Game
+    {
+        public void player()
+        {
+            int playerPosition = 0;
+            Console.WriteLine(" Player Position : "+playerPosition);
+        }
+    }
+}
--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -7,7 +7,8 @@ namespace SnakeLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Snake & Ladder Simulator \n");
-            Console.WriteLine(" Info : This problem simulates a Snake and Ladder Game.\n\tThe Player starts from 0 position. It rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake. \n\tThis goes till the winning spot 100 is achieved.");
+            Game gm = new Game();
+            gm.player();
         }
     }
 }
