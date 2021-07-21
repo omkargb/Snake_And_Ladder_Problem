d05542bc31610f63a4064e1392375a914311fd35
omkargb <omkargb107@gmail.com>
2021-07-21 19:52:58 +0530

    [omkargb] Add : Added program files and welcome message diplayed

---
+++ SnakeLadderProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Snake & Ladder Simulator \n");
+            Console.WriteLine(" Info : This problem simulates a Snake and Ladder Game.\n\tThe Player starts from 0 position. It rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake. \n\tThis goes till the winning spot 100 is achieved.");
+        }
+    }
+}
