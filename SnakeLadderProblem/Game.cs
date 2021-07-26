c21274e2490a9c9791415077205a870c75ce129e
omkargb <omkargb107@gmail.com>
2021-07-26 19:01:20 +0530

    [omkargb] Refractor : Checking for no play, ladder, snake options implemented

--- SnakeLadderProblem/Game.cs
+++ SnakeLadderProblem/Game.cs
@@ -13,7 +13,33 @@ namespace SnakeLadderProblem
             
             Random random = new Random();
             int diceValue = random.Next(1,7);     // generates between 1 to 6
-            Console.WriteLine(" Rolling a die. Value is : "+diceValue);
+            Console.WriteLine(" Rolling a die... Die Value is : "+diceValue);
+
+            Console.WriteLine("\n Deciding options : No Play, Ladder or Snake ...");
+            int option = random.Next(0, 3);     // generates between 0 to 2
+            if (option == 1)
+            {
+                playerPosition = playerPosition + diceValue;
+                Console.WriteLine(" Ladder : +" +diceValue );
+                Console.WriteLine(" Player Position : " + playerPosition);
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine(" Snake : -" + diceValue);
+                if (playerPosition < diceValue)
+                {
+                    playerPosition = 0;
+                }
+                else
+                {
+                    playerPosition = playerPosition - diceValue;
+                }
+                Console.WriteLine(" Player Position : " + playerPosition);
+            }
+            else 
+            {
+                Console.WriteLine(" No Play");
+            }
         }
     }
 }
