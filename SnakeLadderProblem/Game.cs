9a6420857e93875c7dcdd4b595eeac6d2a227dcd
omkargb <omkargb107@gmail.com>
2021-07-26 23:09:45 +0530

    [omkargb] Refractor : Cheking a player is reaching exactly on position 100

--- SnakeLadderProblem/Game.cs
+++ SnakeLadderProblem/Game.cs
@@ -13,36 +13,44 @@ namespace SnakeLadderProblem
 
             while (playerPosition < 100)
             {
-                if (playerPosition>100)
-                {
-                    playerPosition = 100;
-                    Console.WriteLine(" Winning position reached..");
-                }
-
                 int diceValue = random.Next(1, 7);     // generates between 1 to 6
-                Console.Write(" Rolling a die... Die Value is : " + diceValue+ "  | Option");
+                Console.Write(" Rolling a die... Die Value is : " + diceValue);
 
                 int option = random.Next(0, 3);     // generates between 0 to 2
                 if (option == 1)
                 {
                     playerPosition = playerPosition + diceValue;
-                    Console.WriteLine(" --> Ladder : +" + diceValue+ "  |  Player Position : " + playerPosition);
-                }
-                else if (option == 2)
-                {
-                    if (playerPosition < diceValue)
+                    if(playerPosition > 100)
                     {
-                        playerPosition = 0;
+                        playerPosition = playerPosition - diceValue;
+                        Console.WriteLine("  | Option --> Ladder : +" + diceValue + "  |  Keeping current Position : " + playerPosition);
                     }
                     else
                     {
-                        playerPosition = playerPosition - diceValue;
+                        Console.WriteLine("  | Option --> Ladder : +" + diceValue + "  |  Player Position : " + playerPosition);
                     }
-                    Console.WriteLine(" --> Snake  : -" + diceValue+ "  |  Player Position : " + playerPosition);
+                }
+                else if (option == 2)
+                {
+
+                        if (playerPosition < diceValue)
+                        {
+                            playerPosition = 0;
+                        }
+                        else
+                        {
+                            playerPosition = playerPosition - diceValue;
+                        }
+                        Console.WriteLine("  | Option --> Snake  : -" + diceValue + "  |  Player Position : " + playerPosition);
                 }
                 else
                 {
-                    Console.WriteLine(" --> No Play");
+                    Console.WriteLine("  | Option --> No Play");
+                }
+
+                if (playerPosition >= 100)
+                {
+                    Console.WriteLine(" Winning position reached..");
                 }
             }
         }
